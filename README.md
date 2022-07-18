755f8f89e72f3d9227fddf1b278fe9b477a513c9
JMallone <michelgomesdes@hotmail.com>
2022-07-18 13:03:46 -0300

    Adicionado Enviar para Telegram + Criado o arquivo send.py para enviar as ultimas atualizações para algum canal do Telegram

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # 🕵️ Monitor Presidenciáveis Telegram
+
 Bot do Telegram - Para a captura de dados dos canais no Telegram dos Presidenciáveis
 
 ## 📝 Estrutura dos Dados
+
 ![image](https://user-images.githubusercontent.com/6977257/178580578-ced33dda-6617-4633-b268-8ec87fbf64c4.png)
 
-Para as tabelas **telegram_entities** e **telegram_messages** possuímos  'copia' como **\*_changes**
+Para as tabelas **telegram_entities** e **telegram_messages** possuímos 'copia' como **\*\_changes**
 
 ### Colunas
+
 Sendo as colunas comuns para ambas:
 
 - **attr_name:** Indicando o nome de qual coluna foi modificada na tabela
@@ -14,14 +17,17 @@ Sendo as colunas comuns para ambas:
 - **new_value:** O Valor atual
 - **date:** Data de modificação
 
-
 ## 🏃 Como Executar
 
 ### Primeira Configuração
+
 - Execute o comando `pip install -r requirements.txt` para instalar as dependências;
 - Adicione as informações do banco de dados no `config.json`;
 - Troque a variavel `"env": "dev"` para `"env": "prod"` no arquivo `config.json` caso esteja em produção;
 - Execute o comando `python run_configuration.py` para fazer o upload do banco de dados.
 
 ### Capturar/Atualizar os canais monitorados
+
 - Execute o comando `python get_groups.py` para registrar/atualizar novos canais no banco de dados.
+- Execute o comando `python get_new_messages.py` para registrar/atualizar novas mensagens no banco de dados.
+- Execute o comando `python send.py` para mandar os dados para o Telegram/Twitter.
---
+++ send.py
@@ -0,0 +1,99 @@
+
+from sqlalchemy import create_engine, func
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+import json
+from telethon import TelegramClient
+
+from alchemy import Entity, Message, MessageChange
+import conn
+
+with open("config.json") as jsonfile:
+    api = json.load(jsonfile)['telegram']
+
+async def send_to_telegram(mensagem):
+    content = "Nome do Canal: " + mensagem.name if mensagem.name else ""
+    content += ("\nMensagem: " + mensagem.message if hasattr( mensagem, 'message') else "")
+    content += ("\nDe: " + mensagem.old_value if hasattr( mensagem, 'old_value') else "")
+    content += ("\nPara: " + mensagem.new_value if hasattr( mensagem, 'new_value') else "")
+    content += ("\nDeletado em: " + str(mensagem.deleted_at_date) if hasattr( mensagem, 'deleted_at_date') and mensagem.deleted_at_date != None  else "")
+
+    # await client.send_message(354322347, content)
+    await client.send_message('me', content)
+
+    
+
+def send_to_twitter(mensagem):
+    pass
+
+async def send(filter_date):
+    engine = conn.run()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Modificadas e Deletadas
+    query = session.query(
+            MessageChange.message_id, 
+            func.max(MessageChange.date).label('date'), 
+            Entity.name, 
+            MessageChange.attr_name, 
+            MessageChange.old_value, 
+            MessageChange.new_value,
+            Message.deleted,
+            Message.deleted_at_date
+            ).join(
+                Message, Message.id == MessageChange.message_id
+            ).join(
+                Entity, Entity.id == Message.entity_id
+            ).filter(
+                MessageChange.attr_name == 'message',
+                MessageChange.date >= filter_date
+            ).group_by(
+                MessageChange.message_id
+            ).all()
+
+    # Apenas as deletadas
+    query_just_deleted = session.query(
+        Entity.name, 
+        Message.message,
+        Message.deleted_at_date
+        ).outerjoin(
+            MessageChange, 
+            Message.id == MessageChange.message_id
+        ).join(
+            Entity, Entity.id == Message.entity_id
+        ).filter(
+            MessageChange.id == None, 
+            Message.date >= filter_date,
+            Message.deleted != None
+        ).all()
+
+    for row in query:
+        await send_to_telegram(row)
+        send_to_twitter(row)
+
+        print('+------------------------')
+        print("|Grupo {}".format(row.name))
+        print("|\t De:  {} ".format(row.old_value))
+        print("|\t Para:  {} ".format(row.new_value))
+        if row.deleted:
+            print("|\t Deletado em:  {} ".format(row.deleted_at_date))
+        print('+------------------------\n')
+
+    for row in query_just_deleted:
+        await send_to_telegram(row)
+        send_to_twitter(row)
+
+        print('\n+-----------------')
+        print("|Grupo: {}".format(row.name))
+        print("|Mensagem Deletada: {}".format(row.message))
+        print("|Deletado em: {}".format(row.deleted_at_date))
+        print('+----------------\n')
+
+    
+
+client = TelegramClient('anon', api['id'], api['hash'])
+
+with client:
+    client.loop.run_until_complete(send("2022-07-18 12:03:45.000000"))
